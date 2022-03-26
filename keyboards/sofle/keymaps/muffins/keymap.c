db049860206f08b3d564896953cf3515705bf257
Nick Anderson <nanderson7@gmail.com>
2022-03-25 22:59:45 -0700

    muffins: adding my sofle keeb layout

--- keyboards/sofle/keymaps/muffins/keymap.c
+++ keyboards/sofle/keymaps/muffins/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,  KC_5,                               KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,  KC_T,                               KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
   KC_LSFT,  KC_A,    KC_S,     KC_D,     KC_F,  KC_G,                               KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTRL, KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,           KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LGUI(KC_L),
+  KC_LCTL,  KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,           KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LGUI(KC_L),
             KC_MACOS,   KC_LALT,  MO(2), KC_LGUI, KC_DEL,          KC_ENT, KC_SPC,   KC_EQL,  KC_LBRC, KC_RBRC
 ),
 
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,  KC_5,                               KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,  KC_T,                               KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
   KC_LSFT,  KC_A,    KC_S,     KC_D,     KC_F,  KC_G,                               KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTRL, KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,           KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LCTL(LGUI(KC_Q)),
+  KC_LCTL,  KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,           KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LCTL(LGUI(KC_Q)),
             KC_WINDOWS,   KC_LALT,  MO(2), KC_LGUI, KC_DEL,          KC_ENT, KC_SPC,   KC_EQL,  KC_LBRC, KC_RBRC
 ),
 
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_MOD] = LAYOUT(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,                         KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F11,
-  _______, _______, _______, _______, KC_PGDOWN, KC_PGUP,                       KC_HOME,  KC_END,   _______, _______,  _______, KC_F12,
+  _______, _______, _______, _______, KC_PGDN,   KC_PGUP,                       KC_HOME,  KC_END,   _______, _______,  _______, KC_F12,
   _______, _______, _______, _______, _______,   _______,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, _______, _______,
   _______, _______, _______, _______, _______,   _______, _______,     _______, KC_MINUS, KC_GRAVE, _______, _______,  _______, _______,
                     _______, _______, _______,   _______, _______,     _______, _______,  _______,  _______, _______
@@ -206,7 +206,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
 		} else if (index == 1) {
         if (clockwise) {
-          tap_code(KC_PGDOWN);
+          tap_code(KC_PGDN);
         } else {
           tap_code(KC_PGUP);
         }
