ebf0da0cf54ead1cd80934dadc9f25a768cef665
Nick Anderson <nanderson7@gmail.com>
2023-09-03 21:47:49 -0700

    first lulu keymap commit

---
+++ keyboards/boardsource/lulu/keymaps/muffins/keymap.c
@@ -0,0 +1,130 @@
+/* Copyright 2022 Cole Smith <cole@boardsource.xyz>
+ * Copyright 2022 David Rambo <davidrambo@mailfence.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+/* 32 * 14 os logos */
+static const char PROGMEM win_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xbc, 0xbc, 0xbe, 0xbe, 0x00, 0xbe, 0xbe, 0xbf, 0xbf, 0xbf, 0xbf, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x07, 0x0f, 0x0f, 0x00, 0x0f, 0x0f, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+static const char PROGMEM mac_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf0, 0xf6, 0xfb, 0xfb, 0x38, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x0f, 0x1f, 0x1f, 0x0f, 0x0f, 0x1f, 0x1f, 0x0f, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+// enum sofle_layers {
+enum lulu_layers
+    _DEFAULT = 0,
+    _WINDOWS = 0,
+    _MACOS,
+    _MOD,
+};
+
+enum custom_keycodes {
+    KC_WINDOWS = SAFE_RANGE,
+    KC_MACOS,
+	KC_MOD,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Windows
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  Esc |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  BSP |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * | LCTR |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Win+L |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | MO(1)| LGUI | /  DEL  /       \Enter \  |Space |  =   | RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+ [_WINDOWS] = LAYOUT(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MNXT,  KC_MSTP, KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LGUI(KC_L),
+                      KC_LALT, MO(2), KC_LGUI, KC_DEL,                 KC_ENT,  KC_SPC,   KC_EQL,
+),
+
+/*
+ * macOS mode
+ * ,-----------------------------------------.                    ,---------------------------------------------.
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  Bspc    |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+----------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   \      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+----------|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '       |
+ * |------+------+------+------+------+------| Next  |    | Stop  |------+------+------+------+------+----------|
+ * | LCTR |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | Ctl+Cmd+Q|
+ * `-----------------------------------------/       /     \      \---------------------------------------------'
+ *                   | LAlt | MO(1)| LGUI | /  DEL  /       \Enter \  |Space |  =   | RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+[_MACOS] = LAYOUT(
+  KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,  KC_5,                               KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,  KC_T,                               KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
+  KC_LSFT,  KC_A,    KC_S,     KC_D,     KC_F,  KC_G,                               KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTRL, KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,           KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LCTL(LGUI(KC_Q)),
+                           KC_LALT, MO(2), KC_LGUI, KC_DEL,                 KC_ENT,  KC_SPC,   KC_EQL,
+),
+
+/* MOD
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      | PgDwn| PgUp |                    | Home | End  |      |      |      | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      | Copy |Paste |-------.    ,-------| Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|   -  |  `   |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /       /       \      \  |      |      |       |
+ *                   |      |      |      |/       /         \      \ |      |      |       |
+ *                   `----------------------------'            '------''--------------------'
+ */
+[_MOD] = LAYOUT(
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,                         KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F11,
+  _______, _______, _______, _______, KC_PGDOWN, KC_PGUP,                       KC_HOME,  KC_END,   _______, _______,  _______, KC_F12,
+  _______, _______, _______, _______, _______,   _______,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, _______, _______,
+  _______, _______, _______, _______, _______,   _______, _______,     _______, KC_MINUS, KC_GRAVE, _______, _______,  _______, _______,
+                             _______, _______,   _______, _______,     _______, _______,  _______,  _______
+),
+};
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        // Volume control
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        // Page up/Page down
+        if (clockwise) {
+            tap_code(KC_WH_U);
+        } else {
+            tap_code(KC_WH_D);
+        }
+    }
+    return false;
+}
+#endif
