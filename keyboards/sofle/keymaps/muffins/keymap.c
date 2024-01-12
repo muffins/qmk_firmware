5e9dfef5d481db413966178a1f237022f7d318ee
Nick Anderson <nanderson7@gmail.com>
2024-01-12 13:17:53 -0800

    Add RGB toggles and media controls to sofle

--- keyboards/sofle/keymaps/muffins/keymap.c
+++ keyboards/sofle/keymaps/muffins/keymap.c
@@ -26,9 +26,23 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif
 
 /* 32 * 14 os logos */
-static const char PROGMEM win_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xbc, 0xbc, 0xbe, 0xbe, 0x00, 0xbe, 0xbe, 0xbf, 0xbf, 0xbf, 0xbf, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x07, 0x0f, 0x0f, 0x00, 0x0f, 0x0f, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static const char PROGMEM win_logo[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0xbc, 0xbc, 0xbe, 0xbe, 0x00, 0xbe, 0xbe, 0xbf, 0xbf, 0xbf, 0xbf,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07,
+    0x07, 0x0f, 0x0f, 0x00, 0x0f, 0x0f, 0x1f, 0x1f, 0x1f, 0x1f, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
 
-static const char PROGMEM mac_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf0, 0xf6, 0xfb, 0xfb, 0x38, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x0f, 0x1f, 0x1f, 0x0f, 0x0f, 0x1f, 0x1f, 0x0f, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static const char PROGMEM mac_logo[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0xc0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf0, 0xf6, 0xfb, 0xfb, 0x38, 0x10,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+    0x07, 0x0f, 0x1f, 0x1f, 0x0f, 0x0f, 0x1f, 0x1f, 0x0f, 0x06, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
 
 enum sofle_layers {
     _DEFAULT = 0,
@@ -60,11 +74,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'            '------''---------------------------'
  */
 [_WINDOWS] = LAYOUT(
-  KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,  KC_5,                               KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,  KC_T,                               KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
-  KC_LSFT,  KC_A,    KC_S,     KC_D,     KC_F,  KC_G,                               KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTL,  KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,           KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LGUI(KC_L),
-            KC_MACOS,   KC_LALT,  MO(2), KC_LGUI, KC_DEL,          KC_ENT, KC_SPC,   KC_EQL,  KC_LBRC, KC_RBRC
+  KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,  KC_5,                          KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,  KC_T,                          KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
+  KC_LSFT,  KC_A,    KC_S,     KC_D,     KC_F,  KC_G,                          KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTL,  KC_Z,    KC_X,     KC_C,     KC_V,  KC_B, KC_MNXT,      KC_MSTP,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, LGUI(KC_L),
+            KC_MACOS,   KC_LALT,  MO(2), KC_LGUI, KC_DEL,                    KC_ENT, KC_SPC,   KC_EQL,  KC_LBRC, KC_RBRC
 ),
 
 /*
@@ -91,14 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* MOD
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      | PgDwn| PgUp |                    | Home | End  |      |      |      | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      | Copy |Paste |-------.    ,-------| Left | Down |  Up  |Right |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   -  |  `   |      |      |      |      |
+ * ,------------------------------------------.                    ,-----------------------------------------.
+ * |       |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |-------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |RGB_TOG| hue+ | sat+ | bri+ | PgDwn| PgUp |                    | Prev | Play | Next |      |      | F12  |
+ * |-------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |RGBmode| hue- | sat- | bri- | Home | End  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
+ * |-------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |       |      |      |      |      |      |-------|    |-------|   -  |  `   |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      |      |      |      | /       /       \      \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |       |
@@ -106,8 +120,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_MOD] = LAYOUT(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,                         KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F11,
-  _______, _______, _______, _______, KC_PGDN,   KC_PGUP,                       KC_HOME,  KC_END,   _______, _______,  _______, KC_F12,
-  _______, _______, _______, _______, _______,   _______,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, _______, _______,
+  RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_PGDN,   KC_PGUP,                       KC_MPRV, KC_MPLY, KC_MNXT,   _______,  _______, KC_F12,
+  RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_HOME,   KC_END,                        KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, _______, _______,
   _______, _______, _______, _______, _______,   _______, _______,     _______, KC_MINUS, KC_GRAVE, _______, _______,  _______, _______,
                     _______, _______, _______,   _______, _______,     _______, _______,  _______,  _______, _______
 ),
